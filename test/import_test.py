6c7d482e40c2f8cc3022ee890929da82579e2f25
Colm Talbot <colm.talbot@ligo.org>
2021-12-14 16:42:35 +0000

    Move import sampler test to new file/job

--- test/import_test.py
+++ test/import_test.py
@@ -1,12 +1,6 @@
 """
-Verify that imports of Bilby and associated sampler package behave as expected.
-
-This script does two things:
-- tests the number of packages imported with a top-level :code:`import bilby`
-  statement.
-- tests that all of the implemented samplers can be initialized.
-  The :code:`FakeSampler` is omitted as that doesn't require importing
-  any package.
+Tests the number of packages imported with a top-level :code:`import bilby`
+statement.
 """
 
 import sys
@@ -31,13 +25,3 @@ if not unique_packages.isdisjoint(unwanted):
     raise ImportError(
         f"{' '.join(unique_packages.intersection(unwanted))} imported with Bilby"
     )
-
-bilby.core.utils.logger.setLevel("ERROR")
-IMPLEMENTED_SAMPLERS = bilby.core.sampler.IMPLEMENTED_SAMPLERS
-likelihood = bilby.core.likelihood.Likelihood(dict())
-priors = bilby.core.prior.PriorDict(dict(a=bilby.core.prior.Uniform(0, 1)))
-for sampler in IMPLEMENTED_SAMPLERS:
-    if sampler == "fake_sampler":
-        continue
-    sampler_class = IMPLEMENTED_SAMPLERS[sampler]
-    sampler = sampler_class(likelihood=likelihood, priors=priors)
