591255ea65af23db61003772c07c733458ecf1b2
Colm Talbot <colm.talbot@ligo.org>
2021-12-14 16:46:41 +0000

    Force a pymc3 update

--- test/test_samplers_import.py
+++ test/test_samplers_import.py
@@ -4,6 +4,7 @@ Tests that all of the implemented samplers can be initialized.
 The :code:`FakeSampler` is omitted as that doesn't require importing
 any package.
 """
+import bilby
 
 bilby.core.utils.logger.setLevel("ERROR")
 IMPLEMENTED_SAMPLERS = bilby.core.sampler.IMPLEMENTED_SAMPLERS
