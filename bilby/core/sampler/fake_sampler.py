eae4459a42f8d4f9b03d0fb4c944c82d86a33745
Colm Talbot <colm.talbot@ligo.org>
2022-02-08 13:19:13 -0500

    Rework fake sampler

--- bilby/core/sampler/fake_sampler.py
+++ bilby/core/sampler/fake_sampler.py
@@ -1,70 +1,183 @@
+from copy import deepcopy
 
 import numpy as np
 
 from .base_sampler import Sampler
 from ..result import read_in_result
+from ..utils.log import logger
+from ..utils.plotting import latex_plot_format
 
 
 class FakeSampler(Sampler):
-    """
+    r"""
     A "fake" sampler that evaluates the likelihood at a list of
     configurations read from a posterior data file.
 
     See base class for parameters. Added parameters are described below.
+
+    Weights are calculated for each posterior sample as
+
+    .. math::
+
+        w_{i} = \frac{
+            {\cal L}_{\rm new}(d | \theta_{i}) \pi_{\rm new}(\theta_{i})
+        }{
+            {\cal L}_{\rm old}(d | \theta_{i}) \pi_{\rm old}(\theta_{i})
+        }.
+
+    Here :math:`{\cal L}` is the likelihood and :math`\pi` is the prior
+    distribution.
+
+    Following this, the Bayes factor comparing the two models is computed as
+
+    .. math::
+
+        \ln BF = \left< w_{i} \right> +/- \sigma_{w}. \\
+        \sigma^{2}_{w} = \frac{1}{N}
+            \frac{\left< w^{2}_{i} - \left< w_{i} \right>^{2}\right>}
+            {\left< w_{i} \right>^{2}}.
+
+    The posterior samples are rejection sampled, i.e., kept with probability
+    :math:`w_{i}`.
 
     Parameters
     ==========
     sample_file: str
         A string pointing to the posterior data file to be loaded.
     """
-    default_kwargs = dict(verbose=True, logl_args=None, logl_kwargs=None,
-                          print_progress=True)
 
-    def __init__(self, likelihood, priors, sample_file, outdir='outdir',
-                 label='label', use_ratio=False, plot=False,
-                 injection_parameters=None, meta_data=None, result_class=None,
-                 **kwargs):
-        super(FakeSampler, self).__init__(likelihood=likelihood, priors=priors, outdir=outdir, label=label,
-                                          use_ratio=False, plot=False, skip_import_verification=True,
-                                          injection_parameters=None, meta_data=None, result_class=None,
-                                          **kwargs)
+    default_kwargs = dict(verbose=True)
+
+    def __init__(
+        self,
+        likelihood,
+        priors,
+        sample_file,
+        outdir="outdir",
+        label="label",
+        plot=False,
+        **kwargs,
+    ):
+        kwargs["use_ratio"] = False
+        super(FakeSampler, self).__init__(
+            likelihood=likelihood,
+            priors=priors,
+            outdir=outdir,
+            label=label,
+            plot=plot,
+            skip_import_verification=True,
+            **kwargs,
+        )
         self._read_parameter_list_from_file(sample_file)
         self.result.outdir = outdir
         self.result.label = label
+        self.changed_priors = self._old_result.priors != priors
 
     def _read_parameter_list_from_file(self, sample_file):
         """Read a list of sampling parameters from file.
 
         The sample_file should be in bilby posterior HDF5 format.
         """
-        self.result = read_in_result(filename=sample_file)
+        self._old_result = read_in_result(filename=sample_file)
+        self.result = deepcopy(self._old_result)
 
     def run_sampler(self):
         """Compute the likelihood for the list of parameter space points."""
-        self.sampler = 'fake_sampler'
+        likelihood_ratios = list()
+        ln_weights = list()
+        posterior = self.result.posterior[self.priors.keys()]
+        old_ln_likelihoods = self._old_result.log_likelihood_evaluations
 
-        # Flushes the output to force a line break
-        if self.kwargs["verbose"]:
-            print("")
-
-        likelihood_ratios = []
-        posterior = self.result.posterior
-
-        for i in np.arange(posterior.shape[0]):
-            sample = posterior.iloc[i]
+        for ii in np.arange(posterior.shape[0]):
+            sample = posterior.iloc[ii]
 
             self.likelihood.parameters = sample.to_dict()
-            logl = self.likelihood.log_likelihood_ratio()
-            sample.log_likelihood = logl
-            likelihood_ratios.append(logl)
+            old_ln_prob = old_ln_likelihoods[ii]
+            if self.result.use_ratio:
+                new_ln_prob = self.likelihood.log_likelihood_ratio()
+            else:
+                new_ln_prob = self.likelihood.log_likelihood()
+            likelihood_ratios.append(new_ln_prob)
+            sample.log_likelihood = new_ln_prob
+            if self.changed_priors:
+                old_ln_prob += self._old_result.priors.ln_prob(sample)
+                new_ln_prob += self.priors.ln_prob(sample)
+            ln_weights.append(new_ln_prob - old_ln_prob)
 
-            if self.kwargs["verbose"]:
-                print(self.likelihood.parameters['log_likelihood'], likelihood_ratios[-1],
-                      self.likelihood.parameters['log_likelihood'] - likelihood_ratios[-1])
+            logger.debug(
+                f"iteration: {ii}: old ln L: {old_ln_prob}, new ln L: {old_ln_prob}, "
+                f"delta ln L: {new_ln_prob - old_ln_prob}"
+            )
+        ln_weights = np.array(ln_weights)
+        max_ln_ratio = max(ln_weights)
+        normalized = np.exp(ln_weights - max_ln_ratio)
+        ln_bayes_factor = np.log(np.mean(normalized)) + max_ln_ratio
+        variance = (
+            (np.mean(normalized**2) - np.mean(normalized) ** 2)
+            / np.mean(normalized) ** 2
+            / len(ln_weights)
+        )
 
         self.result.log_likelihood_evaluations = np.array(likelihood_ratios)
 
-        self.result.log_evidence = np.nan
-        self.result.log_evidence_err = np.nan
+        keep = normalized > np.random.uniform(0, 1, len(normalized))
+        self.result.posterior = self.result.posterior[keep]
+
+        logger.info(
+            f"Resampling has ln Bayes Factor: {ln_bayes_factor:.2f} +/- {variance ** 0.5:.2f}."
+        )
+        logger.info(f"Keeping {sum(keep)} of {len(keep)} samples.")
+
+        if not all(
+            [self.result.log_bayes_factor is np.nan, self.result.log_evidence is np.nan]
+        ):
+            self.result.log_evidence += ln_bayes_factor
+            self.result.log_bayes_factor += ln_bayes_factor
+            self.result.log_evidence_err = (
+                self.result.log_evidence_err**2 + variance
+            ) ** 0.5
+
+        if self.plot:
+            self.make_comparison_histograms()
+
+        self.result.sampling_time = None
 
         return self.result
+
+    @latex_plot_format
+    def make_comparison_histograms(self):
+        """
+        Plot comparison histograms of the new and old log likelihoods.
+        """
+        import matplotlib.pyplot as plt
+
+        ln_l_old = self._old_result.log_likelihood_evaluations
+        ln_l_new = self.result.log_likelihood_evaluations
+        old_label = self._old_result.label
+        new_label = self.label
+
+        _label = "\\ln {{\\cal L}}_{{\\rm {}}}"
+
+        plt.figure()
+        plt.hist(ln_l_old, bins=50, label=old_label, histtype="step")
+        plt.hist(ln_l_new, bins=50, label=new_label, histtype="step")
+        plt.xlabel(r"$\ln {\cal L}$")
+        plt.legend(loc=2)
+        plt.savefig(f"{self.outdir}/{self.label}_logl.pdf")
+        plt.close()
+
+        plt.figure()
+        delta_logl = ln_l_new - ln_l_old
+        plt.hist(delta_logl, bins=50)
+        plt.xlabel(f"${_label.format(new_label)} - {_label.format(old_label)}$")
+        plt.savefig(f"{self.outdir}/{self.label}_delta_logl.pdf")
+        plt.close()
+
+        plt.figure()
+        delta_logl = np.abs(delta_logl)
+        bins = np.logspace(np.log10(delta_logl.min()), np.log10(delta_logl.max()), 25)
+        plt.hist(delta_logl, bins=bins)
+        plt.xscale("log")
+        plt.xlabel(f"$|{_label.format(new_label)}  - {_label.format(old_label)}|")
+        plt.savefig(f"{self.outdir}/{self.label}_abs_delta_logl.pdf")
+        plt.close()
--- examples/gw_examples/injection_examples/fake_sampler_example.py
+++ examples/gw_examples/injection_examples/fake_sampler_example.py
@@ -4,116 +4,122 @@ Read ROQ posterior and calculate full likelihood at same parameter space points.
 """
 
 import numpy as np
-import deepdish as dd
 import bilby
-import matplotlib.pyplot as plt
 
 
-def make_comparison_histograms(file_full, file_roq):
-    # Returns a dictionary
-    data_full = dd.io.load(file_full)
-    data_roq = dd.io.load(file_roq)
+outdir = "outdir"
+label = "full"
 
-    # These are pandas dataframes
-    pos_full = data_full['posterior']
-    pos_roq = data_roq['posterior']
+np.random.seed(170808)
 
-    plt.figure()
-    plt.hist(pos_full['log_likelihood_evaluations'], 50, label='full', histtype='step')
-    plt.hist(pos_roq['log_likelihood_evaluations'], 50, label='roq', histtype='step')
-    plt.xlabel(r'delta_logl')
-    plt.legend(loc=2)
-    plt.savefig('delta_logl.pdf')
-    plt.close()
+duration = 4
+sampling_frequency = 1024
 
-    plt.figure()
-    delta_dlogl = pos_full['log_likelihood_evaluations'] - pos_roq['log_likelihood_evaluations']
-    plt.hist(delta_dlogl, 50)
-    plt.xlabel(r'delta_logl_full - delta_logl_roq')
-    plt.savefig('delta_delta_logl.pdf')
-    plt.close()
+injection_parameters = dict(
+    chirp_mass=36.0,
+    mass_ratio=0.1,
+    a_1=0.8,
+    a_2=0.3,
+    tilt_1=0.0,
+    tilt_2=0.0,
+    phi_12=1.7,
+    phi_jl=0.3,
+    luminosity_distance=2000.0,
+    theta_jn=0.4,
+    psi=0.659,
+    phase=1.3,
+    geocent_time=1126259642.413,
+    ra=1.375,
+    dec=-1.2108,
+)
 
-    plt.figure()
-    delta_dlogl = np.abs(pos_full['log_likelihood_evaluations'] - pos_roq['log_likelihood_evaluations'])
-    bins = np.logspace(np.log10(delta_dlogl.min()), np.log10(delta_dlogl.max()), 25)
-    plt.hist(delta_dlogl, bins=bins)
-    plt.xscale('log')
-    plt.xlabel(r'|delta_logl_full - delta_logl_roq|')
-    plt.savefig('log_abs_delta_delta_logl.pdf')
-    plt.close()
+waveform_arguments = dict(
+    waveform_approximant="IMRPhenomXPHM",
+    reference_frequency=20.0,
+    minimum_frequency=20.0,
+)
 
+waveform_generator = bilby.gw.WaveformGenerator(
+    duration=duration,
+    sampling_frequency=sampling_frequency,
+    frequency_domain_source_model=bilby.gw.source.lal_binary_black_hole,
+    waveform_arguments=waveform_arguments,
+    parameter_conversion=bilby.gw.conversion.convert_to_lal_binary_black_hole_parameters,
+)
 
-def main():
-    outdir = 'outdir_full'
-    label = 'full'
+ifos = bilby.gw.detector.InterferometerList(["H1", "L1", "V1"])
 
-    np.random.seed(170808)
+ifos.set_strain_data_from_zero_noise(
+    sampling_frequency=sampling_frequency,
+    duration=duration,
+    start_time=injection_parameters["geocent_time"] - 3,
+)
 
-    duration = 4
-    sampling_frequency = 2048
-    noise = 'zero'
+ifos.inject_signal(
+    waveform_generator=waveform_generator, parameters=injection_parameters
+)
 
-    sampler = 'fake_sampler'
-    # This example assumes that the following posterior file exists.
-    # It comes from a run using the full likelihood using the same
-    # injection and sampling parameters, but the ROQ likelihood.
-    # See roq_example.py for such an example.
-    sample_file = 'outdir_dynesty_zero_noise_SNR22/roq_result.h5'
+priors = bilby.gw.prior.BBHPriorDict()
+for key in [
+    "a_1",
+    "a_2",
+    "tilt_1",
+    "tilt_2",
+    "theta_jn",
+    "psi",
+    "ra",
+    "dec",
+    "phi_12",
+    "phi_jl",
+    "luminosity_distance",
+]:
+    priors[key] = injection_parameters[key]
+del priors["mass_1"], priors["mass_2"]
+priors["chirp_mass"] = bilby.core.prior.Uniform(30, 40, latex_label="$\\mathcal{M}$")
+priors["mass_ratio"] = bilby.core.prior.Uniform(0.05, 0.25, latex_label="$q$")
+priors["geocent_time"] = bilby.core.prior.Uniform(
+    injection_parameters["geocent_time"] - 0.1,
+    injection_parameters["geocent_time"] + 0.1,
+    latex_label="$t_c$",
+    unit="s",
+)
 
-    injection_parameters = dict(
-        chirp_mass=36., mass_ratio=0.9, a_1=0.4, a_2=0.3, tilt_1=0.0, tilt_2=0.0,
-        phi_12=1.7, phi_jl=0.3, luminosity_distance=2000., iota=0.4, psi=0.659,
-        phase=1.3, geocent_time=1126259642.413, ra=1.375, dec=-1.2108)
+waveform_generator.waveform_arguments["waveform_approximant"] = "IMRPhenomXP"
 
-    waveform_arguments = dict(waveform_approximant='IMRPhenomPv2',
-                              reference_frequency=20.0, minimum_frequency=20.0)
+likelihood_1 = bilby.gw.GravitationalWaveTransient(
+    interferometers=ifos, waveform_generator=waveform_generator
+)
 
-    waveform_generator = bilby.gw.WaveformGenerator(
-        duration=duration, sampling_frequency=sampling_frequency,
-        frequency_domain_source_model=bilby.gw.source.lal_binary_black_hole,
-        waveform_arguments=waveform_arguments,
-        parameter_conversion=bilby.gw.conversion.convert_to_lal_binary_black_hole_parameters)
+result_1 = bilby.run_sampler(
+    likelihood=likelihood_1,
+    priors=priors,
+    sampler="nestle",
+    nlive=250,
+    injection_parameters=injection_parameters,
+    outdir=outdir,
+    label=f"{label}_XP",
+    save="hdf5",
+)
 
-    ifos = bilby.gw.detector.InterferometerList(['H1', 'L1', 'V1'])
+waveform_generator.waveform_arguments["waveform_approximant"] = "IMRPhenomXPHM"
 
-    if noise == 'Gaussian':
-        ifos.set_strain_data_from_power_spectral_densities(
-            sampling_frequency=sampling_frequency, duration=duration,
-            start_time=injection_parameters['geocent_time'] - 3)
-    elif noise == 'zero':
-        ifos.set_strain_data_from_zero_noise(
-            sampling_frequency=sampling_frequency, duration=duration,
-            start_time=injection_parameters['geocent_time'] - 3)
+likelihood_2 = bilby.gw.GravitationalWaveTransient(
+    interferometers=ifos, waveform_generator=waveform_generator
+)
 
-    ifos.inject_signal(waveform_generator=waveform_generator,
-                       parameters=injection_parameters)
+sample_file = f"{result_1.outdir}/{result_1.label}_result.hdf5"
 
-    priors = bilby.gw.prior.BBHPriorDict()
-    for key in ['a_1', 'a_2', 'tilt_1', 'tilt_2', 'iota', 'psi', 'ra',
-                'dec', 'phi_12', 'phi_jl', 'luminosity_distance']:
-        priors[key] = injection_parameters[key]
-    priors.pop('mass_1')
-    priors.pop('mass_2')
-    priors['chirp_mass'] = bilby.core.prior.Uniform(
-        15, 40, latex_label='$\\mathcal{M}$')
-    priors['mass_ratio'] = bilby.core.prior.Uniform(0.5, 1, latex_label='$q$')
-    priors['geocent_time'] = bilby.core.prior.Uniform(
-        injection_parameters['geocent_time'] - 0.1,
-        injection_parameters['geocent_time'] + 0.1, latex_label='$t_c$', unit='s')
+result_2 = bilby.run_sampler(
+    likelihood=likelihood_2,
+    priors=priors,
+    sampler="fake_sampler",
+    sample_file=sample_file,
+    injection_parameters=injection_parameters,
+    outdir=outdir,
+    label=f"{label}_XPHM",
+    save="hdf5",
+    plot=True,
+    verbose=True,
+)
 
-    likelihood = bilby.gw.GravitationalWaveTransient(
-        interferometers=ifos, waveform_generator=waveform_generator)
-
-    result = bilby.run_sampler(
-        likelihood=likelihood, priors=priors, sampler=sampler, sample_file=sample_file,
-        injection_parameters=injection_parameters, outdir=outdir, label=label)
-
-    # Make a corner plot.
-    result.plot_corner()
-
-    # Compare full and ROQ likelihoods
-    make_comparison_histograms(outdir + '/%s_result.h5' % label, sample_file)
-
-
-if __name__ == '__main__':
-    main()
+bilby.core.result.plot_multiple([result_1, result_2])
