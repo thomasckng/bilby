240dc4d68172420c414fc7ec7116bc66083b5cd1
Colm Talbot <colm.talbot@ligo.org>
2022-05-16 13:00:47 -0700

    rewrite fake sampler example

--- examples/gw_examples/injection_examples/fake_sampler_example.py
+++ examples/gw_examples/injection_examples/fake_sampler_example.py
@@ -1,80 +1,56 @@
 #!/usr/bin/env python
 """
-Read ROQ posterior and calculate full likelihood at same parameter space points.
+Demonstrate using the FakeSampler to reweight a result to include higher-order
+emission modes. This is a simplified version of the method presented in
+arXiv:1905.05477, however, the method can be applied to a much wider range of
+initial and more complex likelihoods.
 """
 
 import bilby
-import deepdish as dd
 import matplotlib.pyplot as plt
 import numpy as np
 
 
-def make_comparison_histograms(file_full, file_roq):
-    # Returns a dictionary
-    data_full = dd.io.load(file_full)
-    data_roq = dd.io.load(file_roq)
-
-    # These are pandas dataframes
-    pos_full = data_full["posterior"]
-    pos_roq = data_roq["posterior"]
+def make_comparison_histograms(result_1, result_2):
+    pos_full = result_1.posterior
+    pos_simple = result_2.posterior
 
     plt.figure()
-    plt.hist(pos_full["log_likelihood_evaluations"], 50, label="full", histtype="step")
-    plt.hist(pos_roq["log_likelihood_evaluations"], 50, label="roq", histtype="step")
+    plt.hist(
+        pos_full["log_likelihood"],
+        50,
+        label=result_1.label,
+        histtype="step",
+        density=True,
+    )
+    plt.hist(
+        pos_simple["log_likelihood"],
+        50,
+        label=result_2.label,
+        histtype="step",
+        density=True,
+    )
     plt.xlabel(r"delta_logl")
     plt.legend(loc=2)
-    plt.savefig("delta_logl.pdf")
-    plt.close()
-
-    plt.figure()
-    delta_dlogl = (
-        pos_full["log_likelihood_evaluations"] - pos_roq["log_likelihood_evaluations"]
-    )
-    plt.hist(delta_dlogl, 50)
-    plt.xlabel(r"delta_logl_full - delta_logl_roq")
-    plt.savefig("delta_delta_logl.pdf")
-    plt.close()
-
-    plt.figure()
-    delta_dlogl = np.abs(
-        pos_full["log_likelihood_evaluations"] - pos_roq["log_likelihood_evaluations"]
-    )
-    bins = np.logspace(np.log10(delta_dlogl.min()), np.log10(delta_dlogl.max()), 25)
-    plt.hist(delta_dlogl, bins=bins)
-    plt.xscale("log")
-    plt.xlabel(r"|delta_logl_full - delta_logl_roq|")
-    plt.savefig("log_abs_delta_delta_logl.pdf")
+    plt.savefig(f"{result_1.outdir}/delta_logl.pdf")
     plt.close()
 
 
 def main():
-    outdir = "outdir_full"
-    label = "full"
+    outdir = "outdir"
 
     np.random.seed(170808)
 
     duration = 4
-    sampling_frequency = 2048
-    noise = "zero"
-
-    sampler = "fake_sampler"
-    # This example assumes that the following posterior file exists.
-    # It comes from a run using the full likelihood using the same
-    # injection and sampling parameters, but the ROQ likelihood.
-    # See roq_example.py for such an example.
-    sample_file = "outdir_dynesty_zero_noise_SNR22/roq_result.h5"
+    sampling_frequency = 1024
 
     injection_parameters = dict(
         chirp_mass=36.0,
-        mass_ratio=0.9,
-        a_1=0.4,
-        a_2=0.3,
-        tilt_1=0.0,
-        tilt_2=0.0,
-        phi_12=1.7,
-        phi_jl=0.3,
+        mass_ratio=0.2,
+        chi_1=0.4,
+        chi_2=0.3,
         luminosity_distance=2000.0,
-        iota=0.4,
+        theta_jn=0.4,
         psi=0.659,
         phase=1.3,
         geocent_time=1126259642.413,
@@ -83,7 +59,7 @@ def main():
     )
 
     waveform_arguments = dict(
-        waveform_approximant="IMRPhenomPv2",
+        waveform_approximant="IMRPhenomXAS",
         reference_frequency=20.0,
         minimum_frequency=20.0,
     )
@@ -98,70 +74,69 @@ def main():
 
     ifos = bilby.gw.detector.InterferometerList(["H1", "L1", "V1"])
 
-    if noise == "Gaussian":
-        ifos.set_strain_data_from_power_spectral_densities(
-            sampling_frequency=sampling_frequency,
-            duration=duration,
-            start_time=injection_parameters["geocent_time"] - 3,
-        )
-    elif noise == "zero":
-        ifos.set_strain_data_from_zero_noise(
-            sampling_frequency=sampling_frequency,
-            duration=duration,
-            start_time=injection_parameters["geocent_time"] - 3,
-        )
-
-    ifos.inject_signal(
-        waveform_generator=waveform_generator, parameters=injection_parameters
+    ifos.set_strain_data_from_zero_noise(
+        sampling_frequency=sampling_frequency,
+        duration=duration,
+        start_time=injection_parameters["geocent_time"] - 2,
     )
 
-    priors = bilby.gw.prior.BBHPriorDict()
+    ifos.inject_signal(
+        waveform_generator=waveform_generator,
+        parameters=injection_parameters,
+    )
+
+    priors = bilby.gw.prior.BBHPriorDict(aligned_spin=True)
     for key in [
-        "a_1",
-        "a_2",
-        "tilt_1",
-        "tilt_2",
-        "iota",
+        "luminosity_distance",
+        "theta_jn",
+        "phase",
         "psi",
         "ra",
         "dec",
-        "phi_12",
-        "phi_jl",
-        "luminosity_distance",
+        "geocent_time",
     ]:
         priors[key] = injection_parameters[key]
-    priors.pop("mass_1")
-    priors.pop("mass_2")
-    priors["chirp_mass"] = bilby.core.prior.Uniform(
-        15, 40, latex_label="$\\mathcal{M}$"
-    )
-    priors["mass_ratio"] = bilby.core.prior.Uniform(0.5, 1, latex_label="$q$")
-    priors["geocent_time"] = bilby.core.prior.Uniform(
-        injection_parameters["geocent_time"] - 0.1,
-        injection_parameters["geocent_time"] + 0.1,
-        latex_label="$t_c$",
-        unit="s",
-    )
+    priors["chirp_mass"].maximum = 45
 
-    likelihood = bilby.gw.GravitationalWaveTransient(
+    likelihood = bilby.gw.likelihood.GravitationalWaveTransient(
         interferometers=ifos, waveform_generator=waveform_generator
     )
 
-    result = bilby.run_sampler(
+    # perform the initial sampling with our simple model
+    original_result = bilby.run_sampler(
         likelihood=likelihood,
         priors=priors,
-        sampler=sampler,
-        sample_file=sample_file,
+        sampler="dynesty",
+        walks=10,
+        nact=3,
+        bound="single",
         injection_parameters=injection_parameters,
         outdir=outdir,
-        label=label,
+        label="primary_mode_only",
+        save="hdf5",
     )
 
-    # Make a corner plot.
-    result.plot_corner()
+    # update the waveform generator to use our higher-order mode waveform
+    likelihood.waveform_generator.waveform_arguments[
+        "waveform_approximant"
+    ] = "IMRPhenomXHM"
 
-    # Compare full and ROQ likelihoods
-    make_comparison_histograms(outdir + f"/{label}_result.h5", sample_file)
+    # call the FakeSampler to compute the new likelihoods
+    new_result = bilby.run_sampler(
+        likelihood=likelihood,
+        priors=priors,
+        sampler="fake_sampler",
+        sample_file=f"{outdir}/{original_result.label}_result.hdf5",
+        injection_parameters=injection_parameters,
+        outdir=outdir,
+        verbose=False,
+        label="higher_order_mode",
+        save="hdf5",
+    )
+
+    # make some comparison plots
+    bilby.core.result.plot_multiple([original_result, new_result])
+    make_comparison_histograms(new_result, original_result)
 
 
 if __name__ == "__main__":
